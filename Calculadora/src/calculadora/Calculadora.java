c26952f091c3a2f7a708cb922defa8bf663ee420
Usuario <Usuario@DESKTOP-EC8KJ6K>
2022-03-14 11:35:05 +0100

    Proyecto en Netbeans simulando una calculadora

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+
+package calculadora;
+
+import java.util.Scanner;
+
+/**
+ * @author Jorge Rodríguez-Caro Molero
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner (System.in);
+        
+        System.out.println("1.Sumar");
+        System.out.println("2.Restar");
+        System.out.println("3.Multiplicar");
+        System.out.println("4.Dividir");
+    }
+}
