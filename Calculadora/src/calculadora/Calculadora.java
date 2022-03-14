9e8635834e5e8b57c298064b7021eb01ec71bd53
Jorge Rodríguez-Caro Molero <>
2022-03-14 12:31:45 +0100

    Version 1.0

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -10,9 +10,36 @@ public class Calculadora {
     public static void main(String[] args) {
         Scanner teclado=new Scanner (System.in);
         
+        int opcion, num1, num2, operacion;
+        
         System.out.println("1.Sumar");
         System.out.println("2.Restar");
         System.out.println("3.Multiplicar");
         System.out.println("4.Dividir");
+        opcion=teclado.nextInt();
+        
+        System.out.println("Introduce el primer número");
+        num1=teclado.nextInt();
+        System.out.println("Introduce el segundo número");
+        num2=teclado.nextInt();
+        
+        switch (opcion){
+            case 1:
+                operacion=num1+num2;
+                System.out.println("La suma de los números es: "+operacion);
+            break;
+            case 2:
+                operacion=num1-num2;
+                System.out.println("La resta de los números es: "+operacion);
+            break;
+            case 3:
+                operacion=num1*num2;
+                System.out.println("El resultado de la multiplicación es: "+operacion);
+            break;
+            case 4:
+                operacion=num1/num2;
+                System.out.println("El resultado de la división es: "+operacion);
+            break;
+        }
     }
 }
